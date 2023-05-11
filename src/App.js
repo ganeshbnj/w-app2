09fd88746b14b8a2316587c4740bf69a5a242ce6
ganeshbnj <32366667+ganeshbnj@users.noreply.github.com>
2023-05-11 16:48:19 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import CityWeather from './CityWeather';
-import Component1 from './Component1';
+//import Component1 from './Component1';
 import Component2 from './Component2';
 
 export default function SimpleAccordion() {
